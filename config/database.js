cd65222e982eab8a3a3bb46cc76c65070452c4c6
Thomas P <xspirits@gmail.com>
2014-03-14 19:29:33 +0100

    fix notifs

--- config/database.js
+++ config/database.js
@@ -1,6 +1,7 @@
 // config/database.js
 module.exports = {
 
-	'url' : 'mongodb://xspirits:221988@ds033429.mongolab.com:33429/heroku_app23022674' // looks like mongodb://<user>:<pass>@mongo.onmodulus.net:27017/Mikha4ot
+	// 'url' : 'mongodb://localhost' // looks like mongodb://<user>:<pass>@mongo.onmodulus.net:27017/Mikha4ot
+'url' : 'mongodb://xspirits:221988@ds033429.mongolab.com:33429/heroku_app23022674' // looks like mongodb://<user>:<pass>@mongo.onmodulus.net:27017/Mikha4ot
 
 };
