7047f98ef7f79d56f27dc0430d901063750453af
Thomas P <xspirits@gmail.com>
2014-03-14 18:22:29 +0100

    db setting for heroku

--- config/database.js
+++ config/database.js
@@ -1,6 +1,6 @@
 // config/database.js
 module.exports = {
 
-	'url' : 'mongodb://localhost' // looks like mongodb://<user>:<pass>@mongo.onmodulus.net:27017/Mikha4ot
+	'url' : 'mongodb://xspirits@gmail.com:thomas88@ds033429.mongolab.com:33429/heroku_app23022674' // looks like mongodb://<user>:<pass>@mongo.onmodulus.net:27017/Mikha4ot
 
 };
