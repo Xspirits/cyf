04b85518146bef75e3ff5f8d5b70869c5147f780
Thomas P <xspirits@gmail.com>
2014-03-20 19:07:24 +0100

    rollback to localhost settings to github

--- config/database.js
+++ config/database.js
@@ -1,8 +1,8 @@
 // config/database.js
 module.exports = {
 
-	// 'url' : 'mongodb://192.168.1.2', 
-	'url' : 'mongodb://xspirits:221988@ds029787.mongolab.com:29787/heroku_app23193994', 
+	'url' : 'mongodb://192.168.1.2', 
+	// 'url' : 'mongodb://xspirits:221988@ds029787.mongolab.com:29787/heroku_app23193994', 
 	'imgurSecret' : '82185b165dcaf5d138c352399b215fe481b1cb00',
 	'imgurClientId' : 'a10cc023eeaf638',
 	google : {
