19dc1e580ab7c1a95c71a47d22ed5daf13d4e8ba
Xspirits <xspirits@gmail.com>
2014-04-04 17:56:04 +0200

    Landing page v0.1 completed. Update signup page.

--- public/js/init.js
+++ public/js/init.js
@@ -5,7 +5,7 @@
  var url = window.location.href;var arr = url.split("/");var result = arr[0] + "//" + arr[2];
  src = [
  'main.js',
- 'completed.js',
+ // 'completed.js',
  // 'challengeBox.js',
  'bootstrap.switch.js',
  'nprogress.js',
